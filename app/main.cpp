1b856b7aa1340b2b87ef15b9f41c72eeb8dc9cd7
leosocy <leosocy@gmail.com>
2018-07-14 01:43:51 +0800

    app architecture

---
+++ app/main.cpp
@@ -0,0 +1,11 @@
+//
+// Created by leosocy on 2018/7/14.
+//
+
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+    std::cout << "Hello, world!" << std::endl;
+    return 0;
+}
---
+++ app/models/README.md
@@ -0,0 +1,1 @@
+# database model and some CRUD api
