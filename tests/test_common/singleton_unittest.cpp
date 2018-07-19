455b09b9165d437a87155249fb94355f676db716
leosocy <leosocy@gmail.com>
2018-07-19 13:15:47 +0800

    add 2 unittests for singleton

--- tests/test_common/singleton_unittest.cpp
+++ tests/test_common/singleton_unittest.cpp
@@ -19,3 +19,20 @@ TEST(SingletonTest, test_singleton_instance_address_equal) {
   auto p2 = Singleton<int>::instance();
   EXPECT_EQ(p1, p2);
 }
+
+class SingletonTest : public Singleton<SingletonTest> {
+public:
+  SingletonTest() = default;
+};
+
+TEST(SingletonTest, test_singleton_inheritance_address_equal) {
+  auto p1 = SingletonTest::instance();
+  auto p2 = SingletonTest::instance();
+  EXPECT_EQ(p1, p2);
+}
+
+TEST(SingletonTest, test_singleton_difference_object_address_equal) {
+  auto p1 = Singleton<int>::instance();
+  auto p2 = SingletonTest::instance();
+  EXPECT_NE((unsigned long*)p1.get(), (unsigned long*)p2.get());
+}
